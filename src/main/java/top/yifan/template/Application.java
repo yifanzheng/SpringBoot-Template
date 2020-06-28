4d8479360a8c9878008cf4b687728f10a179091d
yifanzheng <zhengyifan1996@outlook.com>
2020-06-28 17:46:39 +0800

    [refactor]：取消数据库自动配置

--- src/main/java/top/yifan/template/Application.java
+++ src/main/java/top/yifan/template/Application.java
@@ -1,16 +1,25 @@
 package top.yifan.template;
 
-import org.mybatis.spring.annotation.MapperScan;
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
 @EnableCaching
+@SpringBootApplication(
+        // 如果需要使用数据库，请注释掉以下内容
+        exclude = {
+                DataSourceAutoConfiguration.class,
+                HibernateJpaAutoConfiguration.class,
+                DruidDataSourceAutoConfigure.class
+        }
+)
 public class Application {
     
     public static void main(String[] args) {
